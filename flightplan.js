150a5f0a419e3137d5db05cb81f103dabbcfb9b7
adhikasp <adhika.setyap@gmail.com>
2015-07-31 21:06:47 +0700

    Bypass copy to tmp folder

--- flightplan.js
+++ flightplan.js
@@ -8,7 +8,8 @@ plan.target('production', {
     agent: process.env.SSH_AUTH_SOCK
 });
 
-var tmpDir = 'adhikasetyap.me-' + new Date().getTime();
+var tmpDir  = 'adhikasetyap.me-' + new Date().getTime();
+var prodDir = '/var/www/adhikasetyap.me/public';
 
 // Confirm last gulp build from local
 plan.local(function(local) {
@@ -19,15 +20,15 @@ plan.local(function(local) {
     local.log('Copy files to remote hosts');
     var filesToCopy = local.exec('git ls-files', {silent: true});
 
-    local.transfer(filesToCopy, '/tmp/' + tmpDir);
+    local.transfer(filesToCopy,  + prodDir);
 });
 
 plan.remote(function(remote) {
-    remote.log('Move files to web root');
-    remote.sudo('cp -R /tmp/' + tmpDir + '/* /var/www/adhikasetyap.me/public', {user: 'dhika'});
-    remote.rm('-rf /tmp/' + tmpDir);
+    // remote.log('Move files to web root');
+    // remote.sudo('cp -R /tmp/' + tmpDir + '/* /var/www/adhikasetyap.me/public', {user: 'dhika'});
+    // remote.rm('-rf /tmp/' + tmpDir);
 
     remote.log('Install dependencies');
-    remote.sudo('npm --production install /var/www/adhikasetyap.me/public', {user: 'dhika'});
-    remote.sudo('bower --production install /var/www/adhikasetyap.me/public', {user: 'dhika'});
+    remote.sudo('npm --production install ' + prodDir, {user: 'dhika'});
+    remote.sudo('bower --production install ' + prodDir, {user: 'dhika'});
 });
